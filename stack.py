3a84bc4af12fea46d449ab8b8d6485dad71169ff
chenkaijie1028 <chenkaijie1028@163.com>
2020-04-17 14:38:20 +0800

    ADD stack.py

---
+++ stack.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+    def peek(self):
+        return self.items[self.size() - 1]
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
